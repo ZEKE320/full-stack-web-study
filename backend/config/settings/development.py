b7d0e61741e2ff757ea3c2737bdb36d29c5514a8
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-12-25 08:14:09 +0000

    🎨 インデントやimport文を修正

--- backend/config/settings/development.py
+++ backend/config/settings/development.py
@@ -1,13 +1,13 @@
-from .base import *
+from config.settings.base import *
 
 DATABASES = {
-  "default": {
-    "ENGINE": "django.db.backends.mysql",
-    "NAME": "app",
-    "USER": "root",
-    "PASSWORD": "password",
-    "HOST": "host.docker.internal",
-    "PORT":"53306",
-    "ATOMIC_REQUESTS": True,
-  }
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "app",
+        "USER": "root",
+        "PASSWORD": "password",
+        "HOST": "host.docker.internal",
+        "PORT": "53306",
+        "ATOMIC_REQUESTS": True,
+    }
 }
