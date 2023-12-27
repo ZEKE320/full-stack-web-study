751fabe0f49994eef5c41d9ba900c1f98c8e0284
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-12-27 12:05:28 +0000

    👽️ URL内の単語を複数形で統一

--- backend/api/inventory/urls.py
+++ backend/api/inventory/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path("products/", views.ProductView.as_view()),
     path("products/<int:id>/", views.ProductView.as_view()),
     path("inventories/<int:id>/", views.InventoryView.as_view()),
-    path("purchase/", views.PurchaseView.as_view()),
+    path("purchases/", views.PurchaseView.as_view()),
     path("sales/", views.SaleView.as_view()),
 ]
